64d2401688cd755ea8c7f07ef20a9b25d95c0856
Leo Sjöberg <leo.sjoberg@gmail.com>
2023-01-26 12:38:41 +0200

    support a different HEAD branch

--- pkg/github/tree.go
+++ pkg/github/tree.go
@@ -7,7 +7,7 @@ import (
 )
 
 func (p *Provider) GetFolders() ([]string, error) {
-	tree, _, err := p.client.Git.GetTree(context.Background(), git.SourceOwner, git.SourceRepo, "refs/heads/master", false)
+	tree, _, err := p.client.Git.GetTree(context.Background(), git.SourceOwner, git.SourceRepo, fmt.Sprintf("refs/heads/%s", git.CommitHeadBranch), false)
 	if err != nil {
 		fmt.Printf("Error getting tree: %s", err)
 		return nil, nil
